1bdc4241b6462e391487d5118add9a885b06ae79
bqxtt <1486126243@qq.com>
2021-02-25 17:16:15 +0800

    user handler

--- handler/user.go
+++ handler/user.go
@@ -1,27 +1,68 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ecnuvj/vhoj_api/model/contract"
+	"github.com/ecnuvj/vhoj_api/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func ListUsers(c *gin.Context) {
-
+	request := &contract.ListUsersRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusBadRequest, &contract.ListUsersResponse{
+			BaseResponse: util.NewFailureResponse("request param error, err: %v", err),
+		})
+		return
+	}
 }
 
 func AuthUser(c *gin.Context) {
-
+	request := &contract.AuthUserRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusBadRequest, &contract.AuthUserResponse{
+			BaseResponse: util.NewFailureResponse("request param error, err: %v", err),
+		})
+		return
+	}
 }
 
 func DeleteUser(c *gin.Context) {
-
+	request := &contract.DeleteUserRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusBadRequest, &contract.DeleteUserResponse{
+			BaseResponse: util.NewFailureResponse("request param error, err: %v", err),
+		})
+		return
+	}
 }
 
 func Login(c *gin.Context) {
-
+	request := &contract.LoginRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusBadRequest, &contract.LoginResponse{
+			BaseResponse: util.NewFailureResponse("request param error, err: %v", err),
+		})
+		return
+	}
 }
 
 func Register(c *gin.Context) {
-
+	request := &contract.RegisterRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusBadRequest, &contract.RegisterResponse{
+			BaseResponse: util.NewFailureResponse("request param error, err: %v", err),
+		})
+		return
+	}
 }
 
 func UpdateUserInfo(c *gin.Context) {
-
+	request := &contract.UpdateUserInfoRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusBadRequest, &contract.UpdateUserInfoResponse{
+			BaseResponse: util.NewFailureResponse("request param error, err: %v", err),
+		})
+		return
+	}
 }
--- model/contract/user.go
+++ model/contract/user.go
@@ -1,1 +1,59 @@
 package contract
+
+import "github.com/ecnuvj/vhoj_api/model/entity"
+
+type ListUsersRequest struct {
+	PageNo   int32 `json:"page_no" form:"page_no" binding:"required"`
+	PageSize int32 `json:"page_size" form:"page_size" binding:"required"`
+}
+
+type ListUsersResponse struct {
+	Users        []*entity.User `json:"users"`
+	BaseResponse *BaseResponse  `json:"base_response"`
+}
+
+type AuthUserRequest struct {
+	UserId uint `json:"user_id" form:"user_id" binding:"required"`
+	RoleId uint `json:"role_id" form:"role_id" binding:"required"`
+}
+
+type AuthUserResponse struct {
+	BaseResponse *BaseResponse `json:"base_response"`
+}
+
+type DeleteUserRequest struct {
+	UserId uint `json:"user_id" form:"user_id" binding:"required"`
+}
+
+type DeleteUserResponse struct {
+	BaseResponse *BaseResponse `json:"base_response"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username" form:"username" binding:"required"`
+	Password string `json:"password" form:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	User         *entity.User  `json:"user"`
+	BaseResponse *BaseResponse `json:"base_response"`
+}
+
+type RegisterRequest struct {
+	User *entity.User `json:"user" form:"user" binding:"required"`
+}
+
+type RegisterResponse struct {
+	User         *entity.User  `json:"user"`
+	BaseResponse *BaseResponse `json:"base_response"`
+}
+
+type UpdateUserInfoRequest struct {
+	UserId uint         `json:"user_id" form:"user_Id" binding:"required"`
+	User   *entity.User `json:"user" form:"user" binding:"required"`
+}
+
+type UpdateUserInfoResponse struct {
+	User         *entity.User  `json:"user"`
+	BaseResponse *BaseResponse `json:"base_response"`
+}
